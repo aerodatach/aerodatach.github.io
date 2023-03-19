c70f3eabf5e645fcf055565d65263ae5ccd0c1d8
Your Name <you@example.com>
2023-03-19 15:40:08 +0000

    done

--- js/script.js
+++
@@ -1,6 +0,0 @@
-// Change the second argument to your options:
-// https://github.com/sampotts/plyr/#options
-const player = new Plyr('video', {captions: {active: false}});
-
-// Expose player so it can be used from the console
-window.player = player;
